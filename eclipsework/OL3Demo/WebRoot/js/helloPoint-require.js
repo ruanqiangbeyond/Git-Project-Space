47eb27059002d72f8cf365c5f5522f43b88f533f
ruanqiangbeyond <ruanqiangbeyond@yeah.net>
2015-07-04 19:42:17 +0800

    ol3 demo

---
+++ eclipsework/OL3Demo/WebRoot/js/helloPoint-require.js
@@ -0,0 +1,16 @@
+/**
+ * Created by LaoHuTu on 2015/1/17.
+ */
+goog.require('ol.Feature');
+goog.require('ol.Map');
+goog.require('ol.Overlay');
+goog.require('ol.View');
+goog.require('ol.geom.Point');
+goog.require('ol.layer.Tile');
+goog.require('ol.layer.Vector');
+goog.require('ol.source.TileJSON');
+goog.require('ol.source.Vector');
+goog.require('ol.style.Icon');
+goog.require('ol.style.Fill');
+goog.require('ol.style.Stroke');
+goog.require('ol.style.Style');
